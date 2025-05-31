48feac46ea2b5c2940ce32ca468658ad5d1332dc
naorex <nao23r@gmail.com>
2025-05-31 11:41:06 +0900

    follow update

--- S02UseVariable.java
+++ S02UseVariable.java
@@ -1,5 +1,5 @@
-public class UseVariable {
-    public static void main(String[] args) {
+public class S02UseVariable {
+    public static void main (String[] args) {
         int x;
         x = 6;
         System.out.println(x * x * 3.14); // => 113.04
