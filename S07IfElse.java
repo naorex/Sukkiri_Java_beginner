a350924c17de9d705b0a3b8226eae9d322360238
naorex <nao23r@gmail.com>
2025-06-17 13:05:42 +0000

    today's progress (page 119)

---
+++ S07IfElse.java
@@ -0,0 +1,11 @@
+public class S07IfElse {
+    public static void main (String[] args) {
+        boolean tenki = true;
+        if (tenki == true) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        } else {
+            System.out.println("映画を観ます");
+        }
+    }
+}
---
+++ S08While.java
@@ -0,0 +1,9 @@
+public class S08While {
+    public static void main (String[] args) {
+        boolean doorClose = true;
+        while (doorClose == true) {
+            System.out.println("ノックする");
+            System.out.println("1分待つ");
+        }
+    }
+}
