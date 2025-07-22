382e2633d8db1319a9e4555e16db309d1f35c4e7
naorex <nao23r@gmail.com>
2025-07-22 13:28:01 +0000

    today's progress (page 200)

---
+++ S25Array.java
@@ -0,0 +1,13 @@
+public class S25Array {
+    // int型配列を受け取り、すべての要素を表示するメソッド
+    public static void printArray(int[] array) {
+        for (int element : array) {
+            System.out.println(element);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3};
+        printArray(array); // 配列を渡す（厳密には参照先のポインターを渡す）
+    }
+}
