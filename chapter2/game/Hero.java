3fe6ba734381e0cfcae2c0948658d9be6044dfa4
naorex <nao23r@gmail.com>
2025-07-31 12:39:58 +0000

    today's progress (page 314)

--- chapter2/game/Hero.java
+++ chapter2/game/Hero.java
@@ -1,12 +1,24 @@
 public class Hero {
     String name;
     int hp;
-    public void attack() {}
     public void sleep() {
         this.hp = 100; // this は自身のインスタンスへの変数設定で用いる。（Python でいう self）
         System.out.println(this.name + "は、眠って回復した！");
     }
-    public void sit(int sec) {}
-    public void slip() {}
-    public void run() {}
+    public void sit(int sec) {
+        this.hp += sec; // 秒数分をHP回復
+        System.out.println(
+            this.name + "は、" + sec + "ポイント回復した"
+        );
+    }
+    public void slip() {
+        this.hp -= 5;
+        System.out.println(this.name + "は転んだ！");
+        System.out.println("5のダメージ！");
+    }
+    public void run() {
+        System.out.println(this.name + "は逃げ出した！");
+        System.out.println("GAME OVER");
+        System.out.println("最終HPは" + this.hp + "でした");
+    }
 }
--- chapter2/game/Main.java
+++ chapter2/game/Main.java
@@ -1,3 +1,27 @@
 public class Main {
+    public static void main(String[] args) {
+        // 勇者のインスタンスを生成
+        Hero h = new Hero();
+        // 初期値をセット
+        h.name = "ミナト";
+        h.hp = 100;
+        // System.out.println("勇者" + h.name + "を生み出した！");
 
+        // 敵のインスタンスを生成
+        Matango m1 = new Matango();
+        m1.hp = 50;
+        m1.suffix = 'A';
+
+        Matango m2 = new Matango();
+        m2.hp = 48;
+        m2.suffix = 'B';
+
+        // 勇者クラスのメソッドを呼び出し
+        h.sit(5);
+        h.slip();
+        m1.run();
+        m2.run();
+        h.sit(25);
+        h.run();
+    }
 }
