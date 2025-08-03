a0fc42bb785814f6a9132ed32e9acc9464b39218
naorex <nao23r@gmail.com>
2025-08-03 11:36:17 +0000

    today's progress (page 323)

--- chapter2/game/Main.java
+++ chapter2/game/Main.java
@@ -7,6 +7,12 @@ public class Main {
         h.hp = 100;
         // System.out.println("勇者" + h.name + "を生み出した！");
 
+        // 2つめのインスタンスの挙動
+        Hero h2;
+        h2 = h; // 参照渡し。コピーではない。
+        h2.hp = 200;
+        System.out.println(h.hp); //=> 200。参照渡しが前述で行われているため。
+
         // 敵のインスタンスを生成
         Matango m1 = new Matango();
         m1.hp = 50;
