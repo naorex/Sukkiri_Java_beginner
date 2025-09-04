575000bd78d50370f72e339fa03815bfe3acb494
naorex <nao23r@gmail.com>
2025-09-04 12:44:50 +0000

    UPDATE : today's progress (page 476)

--- chapter7/Hero.java
+++ chapter7/Hero.java
@@ -2,7 +2,8 @@ public class Hero {
     private int hp = 100;
     String name = "名無しヒーロー";
 
-    public void sleep() {
+    void sleep() {
+        // public のアクセス修飾子を付けない場合は package private 修飾子の扱いとなる
         this.hp += 100;
         System.out.println(this.name + "は眠って回復した");
         System.out.println("現在のHPは" + this.hp);
