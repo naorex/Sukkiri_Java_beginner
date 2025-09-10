9b219b136fbfb655c2a032a7b5bbcffdbd17576e
naorex <nao23r@gmail.com>
2025-09-10 12:52:36 +0000

    UPDATE : today's progress (page 503)

--- chapter8/Main.java
+++ chapter8/Main.java
@@ -3,5 +3,15 @@ public class Main {
         Empty e = new Empty();
         String s = e.toString(); // -> どこにも自分では定義していない
         System.out.println(s); // -> しかし、何かしら文字列が出る。Object クラスを継承しているため。
+
+        // Object クラスは全ての大元なので、以下のようにどんなインスタンスも代入できる。
+        Object o1 = new Empty();
+        Object o2 = "こんにちは";
+        Object o3 = 1234;
+
+        Printer p = new Printer();
+        p.printAnything(o1);
+        p.printAnything(o2);
+        p.printAnything(o3);
     }
 }
---
+++ chapter8/Printer.java
@@ -0,0 +1,6 @@
+public class Printer {
+    public void printAnything(Object o) {
+        // どんな型のインスタンスでも受け取って画面表示できるメソッド
+        System.out.println(o.toString());
+    }
+}
