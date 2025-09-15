9f1f7e69882ce5a1eb0c23f144eb68e691ccda25
naorex <nao23r@gmail.com>
2025-09-15 12:46:34 +0000

    UPDATE : today's progress (page 529)

---
+++ chapter9/Main2.java
@@ -0,0 +1,13 @@
+public class Main2 {
+    public static void main(String[] args) {
+        String s1 = "Java and JavaScript";
+        if (s1.contains("Java")) {
+            System.out.println("文字列s1は、Javaを含んでいる");
+        }
+        if (s1.endsWith("Java")) {
+            System.out.println("文字列s1は、Javaが最後尾にある");
+        }
+        System.out.println("文字列s1で最初にJavaが登場する位置は" + s1.indexOf("Java"));
+        System.out.println("文字列s1で最後にJavaが登場する位置は" + s1.lastIndexOf("Java"));
+    }
+}
