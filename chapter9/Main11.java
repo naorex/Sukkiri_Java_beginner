178a7c57c287865e50706c8af86a20ca18aa6d7f
naorex <nao23r@gmail.com>
2025-09-24 12:13:00 +0000

    UPDATE : today's progress (page 573)

---
+++ chapter9/Main11.java
@@ -0,0 +1,16 @@
+import java.time.*;
+
+public class Main11 {
+    public static void main(String[] args) {
+        // LocalDateTimeの生成方法
+        LocalDateTime l1 = LocalDateTime.now();
+        LocalDateTime l2 = LocalDateTime.of(2024, 1, 1, 9, 5, 0, 0);
+        System.out.println(l1);
+        System.out.println(l2);
+
+        // LocalDateTimeとZonedDateTimeの相互変換
+        ZonedDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
+        LocalDateTime l3 = z1.toLocalDateTime();
+        System.out.println(l3);
+    }
+}
